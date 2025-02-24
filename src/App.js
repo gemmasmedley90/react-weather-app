5b16c5789e08d6ec077a0561562c92e4cf64d489
gemmasmedley90 <gemmasmedley90@gmail.com>
2025-02-24 18:12:30 +0000

    updated footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
 import './App.css';
+import React from "react";
 
 export default function App() {
-  return <div className="App">
-    <h1>Weather App</h1>
-    <footer>
-      This project is created by Gemma Smedley and is 
-    <a href="https://github.com/gemmasmedley90/react-weather-app" target="_blank"> open-sourced on Github</a>
-    </footer>
-  </div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by Gemma Smedley and is
+        <a
+          href="https://github.com/gemmasmedley90/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
