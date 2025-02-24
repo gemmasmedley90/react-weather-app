fc11bcda0891523e44b2b532e4eff318cd00c937
gemmasmedley90 <gemmasmedley90@gmail.com>
2025-02-24 18:08:09 +0000

    Added footer with github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,11 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+      This project is created by Gemma Smedley and is 
+    <a href="https://github.com/gemmasmedley90/react-weather-app" target="_blank"> open-sourced on Github</a>
+    </footer>
+  </div>;
 }
