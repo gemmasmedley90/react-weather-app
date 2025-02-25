b0e6c922a225182225c2cf7a2c2481eccd10b07d
gemmasmedley90 <gemmasmedley90@gmail.com>
2025-02-25 18:06:38 +0000

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-
-import './App.css';
+import "./App.css";
 import React from "react";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by Gemma Smedley and is
-        <a
-          href="https://github.com/gemmasmedley90/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
-    </div>
+        <Weather />
+        <footer>
+          This project is created by Gemma Smedley and is
+          <a
+            href="https://github.com/gemmasmedley90/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
